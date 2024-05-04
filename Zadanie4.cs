dc8a193ae526c8ef00f0e784daa057a2a9c55d66
CodeWithEwa <156665178+CodeWithEwa@users.noreply.github.com>
2024-05-04 10:10:00 +0200

    Zadanie4.cs

--- Zadanie4.cs
+++ Zadanie4.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
-using System.IO;
-using System;
-using System.IO;
+
 
 class Program
 {
