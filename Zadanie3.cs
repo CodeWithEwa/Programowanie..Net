9e35dad269bbe41c8dff323de20c55ca6b77861d
CodeWithEwa <156665178+CodeWithEwa@users.noreply.github.com>
2024-05-04 10:05:46 +0200

    Add files via upload

---
+++ Zadanie3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "sciezka/do/pliku.txt";
+
+        try
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+               
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Plik nie został znaleziony.");
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Wystąpił błąd wejścia/wyjścia podczas odczytu pliku.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Wystąpił nieoczekiwany błąd: " + ex.Message);
+        }
+    }
+}
